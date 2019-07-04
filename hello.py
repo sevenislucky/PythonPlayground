1da4db5772adc68446f330ab36d1c15dd31f941d
Kev <kjwilkinson1@sky.com>
2019-07-04 21:57:27 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,4 +7,5 @@ print("Hello world")
 # python3 not really needed but handy if you have multiple python versions
 # followed by the program...
 # create a new file with a .bat extention
-# you 
+# within the file the top line is @ py followed by the path to the file you wish to run
+# folloed by another command like pause.exe
